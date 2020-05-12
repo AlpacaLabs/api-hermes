1606e481b64dcb89cfc6339e5b2dff3d46675614
Kevin Chen <kevinmichaelchen@gmail.com>
2020-05-12 19:56:06 -0400

    Update go-kafka

--- go.mod
+++ go.mod
@@ -4,14 +4,13 @@ go 1.14
 
 require (
 	github.com/AlpacaLabs/go-config v0.0.0-20200512232134-8004548d67af
-	github.com/AlpacaLabs/go-kafka v0.0.0-20200512222731-572ff523fc52
+	github.com/AlpacaLabs/go-kafka v0.0.0-20200512235327-e25621e70b02
 	github.com/AlpacaLabs/protorepo-hermes-go v0.0.0-20200504021841-28421f772c4d
 	github.com/denisenkom/go-mssqldb v0.0.0-20200428022330-06a60b6afbbc // indirect
 	github.com/golang-sql/sqlexp v0.0.0-20170517235910-f1bb20e5a188
-	github.com/golang/protobuf v1.3.3
+	github.com/golang/protobuf v1.4.1
 	github.com/google/uuid v1.0.0
 	github.com/gorilla/mux v1.7.4
-	github.com/segmentio/kafka-go v0.3.6
 	github.com/sfreiberg/gotwilio v0.0.0-20200424172909-47a95c1c632a
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/pflag v1.0.5
--- internal/async/transactional_inbox.go
+++ internal/async/transactional_inbox.go
@@ -2,13 +2,12 @@ package async
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/AlpacaLabs/api-hermes/internal/configuration"
 	"github.com/AlpacaLabs/api-hermes/internal/db"
 	goKafka "github.com/AlpacaLabs/go-kafka"
 	hermesV1 "github.com/AlpacaLabs/protorepo-hermes-go/alpacalabs/hermes/v1"
-	"github.com/golang/protobuf/proto"
-	"github.com/segmentio/kafka-go"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,14 +19,9 @@ func HandleSendEmailRequests(config configuration.Config) {
 
 	groupID := config.AppName
 	topic := "send-email-request"
-
-	reader, closer := goKafka.GetReader(goKafka.GetReaderInput{
-		Host:    config.KafkaConfig.Host,
-		Port:    config.KafkaConfig.Port,
-		GroupID: groupID,
-		Topic:   topic,
-	})
-	defer closer()
+	brokers := []string{
+		fmt.Sprintf("%s:%d", config.KafkaConfig.Host, config.KafkaConfig.Port),
+	}
 
 	dbConn, err := config.SQLConfig.Connect()
 	if err != nil {
@@ -35,7 +29,11 @@ func HandleSendEmailRequests(config configuration.Config) {
 	}
 	dbClient := db.NewClient(dbConn)
 
-	err = goKafka.ProcessKafkaMessages(ctx, reader, handleSendEmailRequest(dbClient))
+	err = goKafka.ProcessKafkaMessages(ctx, goKafka.ProcessKafkaMessagesInput{
+		Brokers: brokers,
+		GroupID: groupID,
+		Topic:   topic,
+	}, handleSendEmailRequest(dbClient))
 	if err != nil {
 		log.Errorf("%v", err)
 	}
@@ -46,14 +44,9 @@ func HandleSendSmsRequests(config configuration.Config) {
 
 	groupID := config.AppName
 	topic := "send-sms-request"
-
-	reader, closer := goKafka.GetReader(goKafka.GetReaderInput{
-		Host:    config.KafkaConfig.Host,
-		Port:    config.KafkaConfig.Port,
-		GroupID: groupID,
-		Topic:   topic,
-	})
-	defer closer()
+	brokers := []string{
+		fmt.Sprintf("%s:%d", config.KafkaConfig.Host, config.KafkaConfig.Port),
+	}
 
 	dbConn, err := config.SQLConfig.Connect()
 	if err != nil {
@@ -61,17 +54,21 @@ func HandleSendSmsRequests(config configuration.Config) {
 	}
 	dbClient := db.NewClient(dbConn)
 
-	err = goKafka.ProcessKafkaMessages(ctx, reader, handleSendSmsRequest(dbClient))
+	err = goKafka.ProcessKafkaMessages(ctx, goKafka.ProcessKafkaMessagesInput{
+		Brokers: brokers,
+		GroupID: groupID,
+		Topic:   topic,
+	}, handleSendSmsRequest(dbClient))
 	if err != nil {
 		log.Errorf("%v", err)
 	}
 }
 
-func handleSendEmailRequest(dbClient db.Client) func(context.Context, kafka.Message) error {
-	return func(ctx context.Context, message kafka.Message) error {
+func handleSendEmailRequest(dbClient db.Client) func(context.Context, goKafka.Message) error {
+	return func(ctx context.Context, message goKafka.Message) error {
 		// Convert kafka.Message to Protocol Buffer
 		pb := &hermesV1.SendEmailRequest{}
-		if err := proto.Unmarshal(message.Value, pb); err != nil {
+		if err := message.Unmarshal(pb); err != nil {
 			return err
 		}
 
@@ -81,11 +78,11 @@ func handleSendEmailRequest(dbClient db.Client) func(context.Context, kafka.Mess
 	}
 }
 
-func handleSendSmsRequest(dbClient db.Client) func(context.Context, kafka.Message) error {
-	return func(ctx context.Context, message kafka.Message) error {
+func handleSendSmsRequest(dbClient db.Client) func(context.Context, goKafka.Message) error {
+	return func(ctx context.Context, message goKafka.Message) error {
 		// Convert kafka.Message to Protocol Buffer
 		pb := &hermesV1.SendSmsRequest{}
-		if err := proto.Unmarshal(message.Value, pb); err != nil {
+		if err := message.Unmarshal(pb); err != nil {
 			return err
 		}
 
